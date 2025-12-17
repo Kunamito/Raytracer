55a3a5861a86af964ca58f41fe91bbec9d49cd15
Ethan GIllaux-Genelle <ethan.gillaux-genelle@epitech.eu>
2025-12-17 19:45:44 +0100

    feat(Utils/): Optimisation

---
+++ include/Utils/Interval.hpp
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2025
+** Raytracing
+** File description:
+** Vector lib for Raytracer
+*/
+
+#include <math.h>
+#include "Utility.hpp"
+
+namespace Raytracer {
+namespace Utils {
+class interval {
+  public:
+    double min, max;
+
+    interval() : min(+infinity), max(-infinity) {} // Default interval is empty
+
+    interval(double min, double max) : min(min), max(max) {}
+
+    double size() const {
+        return max - min;
+    }
+
+    bool contains(double x) const {
+        return min <= x && x <= max;
+    }
+
+    bool surrounds(double x) const {
+        return min < x && x < max;
+    }
+
+    double clamp(double x) const {
+        if (x < min) return min;
+        if (x > max) return max;
+        return x;
+    }
+
+    static const interval empty, universe;
+};
+}
+}
---
+++ include/Utils/Utility.hpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2025
+** Raytracing
+** File description:
+** Vector lib for Raytracer
+*/
+
+#include <limits>
+#include <random>
+
+// Constant
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
+}
